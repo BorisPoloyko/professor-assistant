50cff1cbc206b42b0450299a4b4313071605ce1d
BorisPoloyko <boris.poloiko@gmail.com>
2023-03-31 17:00:10 +0300

    Add Dbcontext and migration

---
+++ src/Identity/DataAccess/Context/StudentsConfig.cs
@@ -0,0 +1,14 @@
+﻿using Identity.Models.Students;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Identity.DataAccess.Context
+{
+    public class StudentsConfig : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.Property(p => p.Id).HasColumnName("TelegramId");
+        }
+    }
+}
